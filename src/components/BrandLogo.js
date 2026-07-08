e57a99242ddf07ae75aafcbf670f9e711004efb7
delgadoruby148-del <delgadoruby148@gmail.com>
2026-07-07 23:45:56 -0400

    increased logo size by 50 percent

--- src/components/BrandLogo.js
+++ src/components/BrandLogo.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-export default function BrandLogo({ className = "h-[52px] w-auto object-contain" }) {
+export default function BrandLogo({ className = "h-[60px] w-auto object-contain" }) {
   const [logoFailed, setLogoFailed] = useState(false);
 
   if (logoFailed) {
